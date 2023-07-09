bb27fce28cec3ddbd1b9331b116d57c3364741cd
Jemini Gaule <gaulejemini@gmail.com>
2023-07-09 18:48:17 -0400

    Added a basic producer and product

---
+++ Products/Product.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var product_name = "Bacon"
+var current_amount = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():"res://Producer.tscn"
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
